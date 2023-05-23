8aeb56786d3cb62a9d0b026aa1e462aedf13f809
Anorboyev-Xayrullo <anorboyevxayrullo841@gmail.com>
2023-05-23 21:10:17 +0500

    main

--- index.js
+++ index.js
@@ -1,78 +1,94 @@
- //    let obj = {
-        //     name : "ali",
-        //     sayHi (){
-        //         console.log(`mening ismim  ${obj.name}`);
-        //     }
-        //    }
+//    let obj = {
+//     name : "ali",
+//     sayHi (){
+//         console.log(`mening ismim  ${obj.name}`);
+//     }
+//    }
 
-        //    obj.sayHi()
+//    obj.sayHi()
 
-        // let obj = {
-        //     name: 'ali'
-        // }
-        // function sayHello() {
-        //     let other = {
-        //      name: "a"
-        //     }
-        //     console.log(other.name);
-        // }
-        // sayHello(this.name);
-
-
-        // let a = +prompt("raqam kiriting")
-        // let obj = {
-        //     name : 'ali',
-        //     age : a ,
-        //     oddYears(){
-        //     if (a %  2 === 0){
-        //         console.log("juft son");
-        //     }else{
-        //         console.log("toq son");
-        //     }
-        //     }
-        //     }
-
-        // obj.oddYears()
-       
-       
-        // fetch("url")
-        // .then(res => res.json())
-        // .then((data) =>{
-        //     data.forEach((elem,index,arr) => {
-        //         console.log(elem);
-        //         console.log(index);
-        //         console.log(arr);
-        //     });
-        // })
-        
-        let row = document.querySelector(".row")
-       
-        let fragment = new DocumentFragment()
-
-        fetch('https://jsonplaceholder.typicode.com/photos')
-        .then(res=> res.json())
-        .then(data=>{
-            data.forEach((values,index)=>{
-                 if(values.id == 100) return true;
-                 console.log(values);
-                const divEl = document.createElement("div")
-                const h1EL = document.createElement("h1")
-                divEl.setAttribute("class","col-3")
-                const imgEl = document.createElement("img");
-                h1EL.textContent = values.id
-                imgEl.src = values.url;
-
-                divEl.appendChild(h1EL)
-                divEl.appendChild(imgEl)
-
-                row.appendChild(divEl)
-            })
-        })
+// let obj = {
+//     name: 'ali'
+// }
+// function sayHello() {
+//     let other = {
+//      name: "a"
+//     }
+//     console.log(other.name);
+// }
+// sayHello(this.name);
 
 
-        // row.appendChild(fragment)
+// let a = +prompt("raqam kiriting")
+// let obj = {
+//     name : 'ali',
+//     age : a ,
+//     oddYears(){
+//     if (a %  2 === 0){
+//         console.log("juft son");
+//     }else{
+//         console.log("toq son");
+//     }
+//     }
+//     }
+
+// obj.oddYears()
+
+
+// fetch("url")
+// .then(res => res.json())
+// .then((data) =>{
+//     data.forEach((elem,index,arr) => {
+//         console.log(elem);
+//         console.log(index);
+//         console.log(arr);
+//     });
+// })
+
+// let row = document.querySelector(".row")
+
+// let fragment = new DocumentFragment()
+
+// fetch('https://jsonplaceholder.typicode.com/photos')
+// .then(res=> res.json())
+// .then(data=>{
+//     data.forEach((values,index)=>{
+//          if(values.id == 100) return true;
+//          console.log(values);
+//         const divEl = document.createElement("div")
+//         const h1EL = document.createElement("h1")
+//         divEl.setAttribute("class","col-3")
+//         const imgEl = document.createElement("img");
+//         h1EL.textContent = values.id
+//         imgEl.src = values.url;
+
+//         divEl.appendChild(h1EL)
+//         divEl.appendChild(imgEl)
+
+//         row.appendChild(divEl)
+//     })
+// })
+
+
+// row.appendChild(fragment)
 // 
 
-    
-        // for(let i= 0; i<100; i++){
-        //     
+
+// for(let i= 0; i<100; i++){
+//     
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+
+
+
+
